2734e934ceddd03e54680d879c7cd55383ad0468
klemo1997 <matuskoklemo@gmail.com>
2021-12-16 17:39:59 +0100

    test: add counter decrement spec test

--- src/domain/counterModel.spec.ts
+++ src/domain/counterModel.spec.ts
@@ -1,4 +1,4 @@
-import {create} from "./counterModel";
+import {create, decrement} from "./counterModel";
 
 describe('create', () => {
     it('creates counter with given value', () => {
@@ -7,5 +7,33 @@ describe('create', () => {
 
     it('creates counter with another value', () => {
         expect(create(2).value).toBe(2)
+    });
+})
+
+describe('decrement', () => {
+    it('does not mutate counter instance', () => {
+        const counter = create(1)
+
+        decrement(counter)
+
+        expect(counter.value).toBe(1)
+    });
+
+    it('decreases value of counter by 1', () => {
+        const counter = create(1)
+
+        expect(decrement(counter).value).toBe(0)
+    });
+
+    it('decreases another value by 1', () => {
+        const counter = create(5)
+
+        expect(decrement(counter).value).toBe(4)
+    });
+
+    it('does not decrement when counter value is zero', () => {
+        const counter = create(0)
+
+        expect(decrement(counter).value).toBe(0)
     });
 })
